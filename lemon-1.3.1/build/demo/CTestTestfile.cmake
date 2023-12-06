df564951999a86354267085cc7956cd511514ffa
Bryan Han <bryan.han77@gmail.com>
2023-12-06 16:33:09 -0500

    branch and cut

---
+++ lemon-1.3.1/build/demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bryanhan77/cs136/lemon-1.3.1/demo
+# Build directory: /home/bryanhan77/cs136/lemon-1.3.1/build/demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ lemon-1.3.1/build/lemon/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bryanhan77/cs136/lemon-1.3.1/lemon
+# Build directory: /home/bryanhan77/cs136/lemon-1.3.1/build/lemon
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
